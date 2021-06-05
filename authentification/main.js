82410e5c796c529cbd750f33382e585c654fc774
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2021-06-05 12:06:31 +0200

    modifications ports

---
+++ authentification/main.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const userRoute = require('./routes/Auth');
+
+app.use( '/auth', userRoute)
+
+app.get('/',(req, res) => {
+    res.set('Content-Type', 'text/html');
+    res.send('Auth');
+})
+
+app.listen(3000, () => {
+    console.log('Server app listening on port ' + port);
+});
---
+++ stock/main.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req, res) => {
+    res.set('Content-Type', 'text/html');
+    res.send('Stock');
+})
+
+app.listen(3000, () => {
+    console.log('Server app listening on port ' + 3000);
+});
