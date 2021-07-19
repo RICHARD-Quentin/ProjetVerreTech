f6a1dcb1440a39039c62c130a1c1cb2f39abda68
florian stock <florian0stock@gmail.com>
2021-07-19 20:21:45 +0200

    Modifications module db + example

--- common/database/index.js
+++ common/database/index.js
@@ -2,12 +2,13 @@ const fs = require('fs');
 const Sequelize = require("sequelize"); 
 const initModels = require("./models/init-models");
 
-let rawdata = fs.readFileSync("config/config.json");
+console.log(__dirname)
+let rawdata = fs.readFileSync(__dirname+"\\config\\config.json");
 let data = JSON.parse(rawdata);
 
 var sequelize = new Sequelize(data.development);
-
 var models = initModels(sequelize);
 
-module.exports.default = models;
+
+module.exports = models;
 
--- order/main.js
+++ order/main.js
@@ -1,10 +1,11 @@
-const express = require('express');
+import Database from 'database';
+import express from 'express';
+import router from './routes.js';
+
 const app = express();
 
-app.get('/',(req, res) => {
-    res.set('Content-Type', 'text/html');
-    res.send('Commande');
-})
+app.use(router);
+
 
 app.listen(3000, () => {
     console.log('Server app listening on port ' + 3000);
