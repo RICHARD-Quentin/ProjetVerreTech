464c111d75374eef1a2c8b7ce9ebd9c694013c37
florian stock <florian0stock@gmail.com>
2021-08-14 22:53:56 +0200

    Modification du module database pour utiliser sequelize directement (transactions ect...)

--- common/database/index.js
+++ common/database/index.js
@@ -2,13 +2,11 @@ const fs = require('fs');
 const Sequelize = require("sequelize"); 
 const initModels = require("./models/init-models");
 
-console.log(__dirname)
 let rawdata = fs.readFileSync(__dirname+"\\config\\config.json");
 let data = JSON.parse(rawdata);
 
 var sequelize = new Sequelize(data.development);
 var models = initModels(sequelize);
 
-
 module.exports = models;
-
+module.exports.Sequelize = sequelize;
