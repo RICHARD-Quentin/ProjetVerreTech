3eb283a8bfe29d67a0309a23bba7681ee5f6bc5a
florian stock <florian0stock@gmail.com>
2021-10-02 14:31:53 +0200

    mise en place payment

--- payement/main.js
+++ payement/main.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(req, res) => {
-    res.set('Content-Type', 'text/html');
-    res.send('Paiement');
-})
+const app = express();
+
+app.use(express.json())
+app.use(router);
+
 
 app.listen(3000, () => {
     console.log('Server app listening on port ' + 3000);
