0be704eddfe76391c5b4d80201fb39462219cebc
florian stock <florian0stock@gmail.com>
2021-12-14 21:15:50 +0100

    mise en place des routes

--- catalog/src/routes/catalog.ts
+++ catalog/src/routes/catalog.ts
@@ -4,19 +4,26 @@ import {validationResult} from 'express-validator';
 import { checkSchema } from 'express-validator';
 import { Type } from 'typescript';
 import ArticleController from '../controllers/ArticleController';
+import ShopController from '../controllers/ShopController';
 import { ArticleShema } from '../models/ArticleShema';
+import { ShopShema } from '../models/ShopShema';
 const router = express.Router();
 
 const articleController = new ArticleController();
-/*
-router.route('/article').post(checkSchema(OrderSchema))
-router.route('/article').put(checkSchema(OrderSchema))
-router.route('/article/:id').delete(checkSchema(OrderSchema))
-*/
 
+router.route('/article').get(articleController.GetArticles)
+router.route('/article/:id').get(articleController.GetArticle)
+router.route('/article/:id').put(checkSchema(ArticleShema),articleController.UpdateArticle)
+router.route('/article/:id').delete(articleController.RemvoveArticle)
+router.route('/article/all').delete(articleController.RemoveAll)
 router.route('/article').post(checkSchema(ArticleShema),articleController.CreateArticle)
 
-router.route('/article').get(articleController.GetArticle)
-//router.route('/article/:id').get()
+const shopController = new ShopController()
+
+router.route('/shop').get(shopController.GetShops)
+router.route('/shop/:id').get(shopController.GetShop)
+router.route('/shop/:id').put(checkSchema(ShopShema),shopController.UpdateShop)
+router.route('/shop/:id').delete(shopController.RemoveShop)
+router.route('/shop').post(checkSchema(ShopShema),shopController.CreateShop)
 
 export default router;
