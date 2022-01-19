e6e36e3e1a064408d18ceb1c7d8f34b39243c0e7
florian stock <florian0stock@gmail.com>
2022-01-19 13:58:16 +0100

    modifs

--- commands.sh
+++ commands.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 # Utils group executor.sh
 
-<<<<<<< Updated upstream
 #Paths services
 services_path=("catalog" "logistic" "users" "common/auth" "common/database")
-=======
-services_path=("catalog" "logistic" "users")
->>>>>>> Stashed changes
 
 for str in ${services_path[@]}; do
     cd $PWD/$str 
