cfec3cdec6dc7c9cae7a7c0f6acf6877069998b6
Quentin <richard.quentin88@gmail.com>
2022-05-03 13:14:54 +0200

    update

--- execute.sh
+++ execute.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Utils group executor.sh
 
-services_path=("catalog" "logistic" "users" "cache" "common/auth" "common/database" "common/controllers" "common/interfaces")
+services_path=("catalog" "logistic" "users" "cache" "common/auth" "database" "controllers" "interfaces")
 
 for str in ${services_path[@]}; do
     cd $PWD/$str 
