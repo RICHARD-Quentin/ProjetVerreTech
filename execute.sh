81fccec781339427a4b23dab74039a8203d89887
Quentin <richard.quentin88@gmail.com>
2022-05-03 12:03:04 +0200

    fix pckg-lock

--- execute.sh
+++ execute.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Utils group executor.sh
 
-services_path=("catalog" "logistic" "users")
+services_path=("catalog" "logistic" "users" "cache" "common/auth" "common/database" "common/controllers" "common/interfaces")
 
 for str in ${services_path[@]}; do
     cd $PWD/$str 
