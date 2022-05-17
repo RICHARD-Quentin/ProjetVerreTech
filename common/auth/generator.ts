5c0918d2cf9adaf428536257e5ba14a405fc8ad8
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2022-05-17 16:40:05 +0200

    Update generator.ts

--- common/auth/generator.ts
+++ common/auth/generator.ts
@@ -1,12 +1,12 @@
 import axios from 'axios'
 import jwt, { RequestHandler } from "express-jwt";
 
-const token_url:string = "https://fenrirproject.eu.auth0.com/oauth/token"
+const token_url:string = "https://kradihsoy.eu.auth0.com/oauth/token"
 
 export default async function() {
     const res = await axios.post(token_url, {        
-            client_id: "W8z4L6ey39u556desXZrPupIpZAW76m7",
-            client_secret:"nudC6rSwcu33iVcgfWSKFPTFcsQne87x2BLlWJ7MHynxMjX2QrC4hKhup1DpRUga",
+            client_id: "Vk9bvxeU0pYFmBNZuQ1gNawJgoE1tzYW",
+            client_secret:"DwqI1plaEy7Mtq0gaHgWcxxN_-jFLbPd-rG15IkmBzyN6I98p_7vKRtWVHjZOX6B",
             audience:"https://verretech/api",
             grant_type:"client_credentials"          
     })
